5d1e08f8780ada0703a25f69c44b819dba2d6d23
ivivanov18 <ivanov.ivan.v@gmail.com>
2019-05-13 00:56:15 +0300

    add show-cart route

--- routes/cart.js
+++ routes/cart.js
@@ -6,15 +6,23 @@ router.get("/", (req, res) => {
   res.send("CART ROUTE");
 });
 
+// TODO: make routes protected
 router.get("/add-to-cart/:id", (req, res) => {
   const productId = req.params.id;
-  const cart = new Cart(req.session.cart ? req.session.cart : {});
+  let cart = new Cart(req.session.cart ? req.session.cart : {});
   // TODO: check product exists by id and then add
   cart.add({ item: "laptop", price: 10 }, productId);
   cart.displayCart();
-  req.session.cart = { ...cart };
-  console.log("req.session.cart", req.session.cart);
-  return res.send({ cart: req.session.cart });
+  req.session.cart = cart.toObjectMapping();
+  res.json({ ...cart.toObjectMapping() });
+});
+
+router.get("/get-cart", (req, res) => {
+  if (!req.session.cart) {
+    return res.json({ items: {}, totalQty: 0, totalPrice: 0 });
+  }
+  const cart = new Cart(req.session.cart);
+  return res.json({ ...cart.toObjectMapping() });
 });
 
 module.exports = router;
