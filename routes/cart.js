1dcfeb24c48bc045b058883d417382ce7382e49b
ivivanov18 <ivanov.ivan.v@gmail.com>
2019-05-13 19:23:18 +0300

    feat: add check whether product exist in db before updating cart

--- routes/cart.js
+++ routes/cart.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const Cart = require("../models/Cart");
-
-router.get("/", (req, res) => {
-  res.send("CART ROUTE");
-});
+const Product = require("../models/Product");
 
 // TODO: make routes protected
-router.get("/add-to-cart/:id", (req, res) => {
+router.get("/add-to-cart/:id", async (req, res) => {
   const productId = req.params.id;
   let cart = new Cart(req.session.cart ? req.session.cart : {});
-  // TODO: check product exists by id and then add
-  cart.add({ item: "laptop", price: 10 }, productId);
+
+  const productInDB = await Product.findById(productId);
+  if (productInDB === null) {
+    return res.status(404).json({ error: "product not found" });
+  }
+
+  cart.add({ item: productInDB.item, price: productInDB.price }, productId);
   cart.displayCart();
   req.session.cart = cart.toObjectMapping();
   res.json({ ...cart.toObjectMapping() });
