12246a125408bf06bacd72f5d9b1d7a62528dcbe
ivivanov18 <ivanov.ivan.v@gmail.com>
2019-05-13 19:24:35 +0300

    feat: add endpoints to get all products and get products by category

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const Product = require("../models/Product");
 
 // @route GET /products
-// @desc Test route
+// @desc
 // @access Public
-router.get("/", (req, res) => res.send("Products Routes"));
+router.get("/", async (req, res) => {
+  const result = await Product.getProducts();
+  if (!result) {
+    return res.send("There are no books corresponding to your research");
+  }
+  res.json({ result });
+});
+
+// @route GET /products/by-category
+// @desc
+// @access Public
+router.get("/by-category", async (req, res) => {
+  //const category = req.params.byCategory;
+  const { category } = req.body;
+  const result = await Product.getProductsBy(category);
+  if (!result) {
+    return res.send("There are no books corresponding to your research");
+  }
+  res.json({ result });
+});
 
 module.exports = router;
