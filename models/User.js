51a9f47089708ce13acff240e8af87ed58193a48
ivivanov18 <ivanov.ivan.v@gmail.com>
2019-05-13 13:10:57 +0300

    modify collection name , and add collectionName argument to getConnection function

--- models/User.js
+++ models/User.js
@@ -1,21 +1,33 @@
 const ObjectId = require("mongodb").ObjectID;
 
 const connection = require("../db/connection");
-const userCollectionName = "users";
+const USERS_COLLECTION = "users";
 
+/**
+ * @access private
+ */
 let _name = new WeakMap();
 let _email = new WeakMap();
 let _password = new WeakMap();
 let _id = new WeakMap();
 
 class User {
-  constructor(payload) {
-    _name.set(this, payload.name);
-    _email.set(this, payload.email);
-    _password.set(this, payload.password);
+  /**
+   * @constructor
+   * @param {string} name - of the user
+   * @param {string} email - of the user
+   * @param {string} password - of the user
+   */
+  constructor({ name, email, password }) {
+    _name.set(this, name);
+    _email.set(this, email);
+    _password.set(this, password);
     _id.set(this, new ObjectId());
   }
 
+  /**
+   *
+   */
   toMongoBinding() {
     return {
       _id: _id.get(this),
@@ -31,7 +43,7 @@ class User {
       acc[curr] = 0;
       return acc;
     }, {});
-    const collection = await getConnection();
+    const collection = await getConnection(USERS_COLLECTION);
     const user = await collection.findOne(
       { _id: new ObjectId(id) },
       { fields: { ...filteredFieldsObj } }
@@ -41,13 +53,17 @@ class User {
   }
 
   static async findOne({ email }) {
-    const collection = await getConnection();
+    const collection = await getConnection(USERS_COLLECTION);
     const user = await collection.findOne({ email });
     return user;
   }
 
+  /**
+   * function to save the user in the db
+   * @returns {object} result - of the insertion operation
+   */
   async save() {
-    const collection = await getConnection();
+    const collection = await getConnection(USERS_COLLECTION);
     const result = await collection.insertOne(this.toMongoBinding());
 
     return result;
@@ -66,9 +82,9 @@ class User {
   }
 }
 
-const getConnection = async () => {
+const getConnection = async collectionName => {
   const conn = await connection();
-  return await conn.db().collection(userCollectionName);
+  return await conn.db().collection(collectionName);
 };
 
 module.exports = User;
